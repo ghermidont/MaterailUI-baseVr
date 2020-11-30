ab3f3bfda60a3a19fb596e35cd264567abb99b1c
ghermidon <mihai_olaru@ymail.com>
2020-11-30 13:45:11 +0200

    structured header. TO DO: add header menu an middle text.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,11 +3,12 @@ import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import classNames from 'classnames';
 //import Button from '@material-ui/core/Button';
-//import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
 //import Icon from '@material-ui/core/Icon';
 //import DeleteIcon from '@material-ui/icons/Delete';
 //import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-//import Paper from '@material-ui/core/Paper';
+import Paper from '@material-ui/core/Paper';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 // https://material-ui.com/components/menus/
 // https://material-ui.com/components/buttons/#text-buttons
@@ -21,17 +22,28 @@ import button1BgImage from '../../assets/png/1.1.png';
 
 const useStyles = makeStyles(theme => ({
     root: {
-        display: 'flex',
-        alignItems: 'center',
+        flexGrow: 1,
+        //display: 'flex',
+        //alignItems: 'center',
         minWidth: 300,
-        height: '15rem',
-        position:'relative',
-        alignContent: 'center',
+        //height: '15rem',
+        //position:'relative',
+        //alignContent: 'center',
         backgroundImage: `url(${headerBgImage})`,
         backgroundSize: '100% 100%',
         backgroundColor: '#2c3503',
         backgroundRepeat: 'no-repeat',
+        direction: 'row',
+        justify: 'center',
+        alignItems: 'center',
     },
+
+    gridButtons:{
+        margin: '0 auto',
+        display: 'block',
+        textAlign: 'center'
+    },
+    
     buttons: {
         margin: theme.spacing(1),
         backgroundSize: '1.5rem',
@@ -47,14 +59,10 @@ const useStyles = makeStyles(theme => ({
         backgroundRepeat: 'no-repeat',
         //margin: '9rem 1rem 10rem 0rem',
     },
-    buttonDiv:{
-        margin: 0,
-        position: 'absolute',
-        top: '50%',
-        left: '18%',
-        right: '18%'
-        //transform: 'translate(-50%, -50%)'
-
+    authButton:{
+        borderRadius: '20px',
+        float: 'right',
+        color: 'white'
     },
     button1: {
         backgroundImage: `url(${button1BgImage})`,
@@ -74,9 +82,9 @@ const useStyles = makeStyles(theme => ({
     button6: {
         backgroundImage: `url(${button1BgImage})`
     },
-    bottomDiv: {
-        height: '100px',
-    }
+    //bottomDiv: {
+    //height: '100px',
+    //}
 }));
 
 
@@ -87,16 +95,27 @@ export default function Header() {
     return (
         <React.Fragment>
             <div className={classes.root}>
-                <div className={classes.buttonDiv}>
-                    <Button className={classNames(classes.buttons, classes.button1)} variant="contained">Certificat de Urbanism</Button>
-                    <Button className={classNames(classes.buttons, classes.button2)} variant="contained">Reserve city tour</Button>
-                    <Button className={classNames(classes.buttons, classes.button3)} variant="contained">Obtain permits</Button>
-                    <Button className={classNames(classes.buttons, classes.button4)} variant="contained">Find the best</Button>
-                    <Button className={classNames(classes.buttons, classes.button5)} variant="contained">Return to you</Button>
-                    <Button className={classNames(classes.buttons, classes.button6)} variant="contained">Reserve tours</Button>
-                </div>
-            </div>
+                <Grid container spacing={3} direction="row" justify="center" alignItems="center">
+                    <Grid item xs={12} sm={6}>
+                        <Paper className={classes.paper}>xs=12 sm=6</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={6} direction="row" justify="flex-end" alignItems="center">
+                        <Button className={classes.authButton} color="primary" variant="outlined" href="#outlined-buttons" startIcon={<ExitToAppIcon />}>Autentificare</Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>xs=12</Paper>
+                    </Grid>
 
+                    <Grid className={classes.gridButtons} item xs={12} >
+                        <Button className={classNames(classes.buttons, classes.button1)} variant="contained">Certificat de Urbanism</Button>
+                        <Button className={classNames(classes.buttons, classes.button2)} variant="contained">Reserve city tour</Button>
+                        <Button className={classNames(classes.buttons, classes.button3)} variant="contained">Obtain permits</Button>
+                        <Button className={classNames(classes.buttons, classes.button4)} variant="contained">Find the best</Button>
+                        <Button className={classNames(classes.buttons, classes.button5)} variant="contained">Return to you</Button>
+                        <Button className={classNames(classes.buttons, classes.button6)} variant="contained">Reserve tours</Button>
+                    </Grid>
+                </Grid>
+            </div>
         </React.Fragment>
     );
 }
